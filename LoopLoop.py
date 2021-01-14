fe33542cc6ab9bc78dbfa608b5b97863344ac6b5
outofthenorm27 <ediscoverypro@gmail.com>
2021-01-13 23:10:30 -0500

    completed LoopLoop practice

---
+++ LoopLoop.py
@@ -0,0 +1,28 @@
+# Loop through a range of numbers (0 through 5)
+# numbers = range(5)
+# for x in numbers:
+#     print(x)
+
+# "for each item in the list of items":
+#     "Do something"
+
+# numbers = range(2,7)
+# for x in numbers:
+#     print(x)
+
+# Iterate throug letters in a string
+# word = "Hello"
+# for letter in word:
+#     print(letter)
+
+# Iterate through a list
+# names = ["Eric", "Dexter","Mike"]
+# for name in names:
+#     print(name)
+
+# Loop while a condition is being met
+run = "y"
+
+while run == "y":
+    print("What's Up?")
+    run = input("To run again. Enter 'y': ")
